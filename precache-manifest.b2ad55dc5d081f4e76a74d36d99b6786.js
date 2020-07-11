49188a944f3691b8b20fbd66fb803fe0fcd82c19
Mahir Köle <mahirkole@gmail.com>
2020-07-12 00:15:49 +0300

    Updates

---
+++ precache-manifest.b2ad55dc5d081f4e76a74d36d99b6786.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11d0d71ca6932c09e7ec3cbff469141",
+    "url": "/reactToDoApp/index.html"
+  },
+  {
+    "revision": "b143180dbff2f547426b",
+    "url": "/reactToDoApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "949f1e9c44839d49b122",
+    "url": "/reactToDoApp/static/js/2.966575f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactToDoApp/static/js/2.966575f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b143180dbff2f547426b",
+    "url": "/reactToDoApp/static/js/main.1514bf5e.chunk.js"
+  },
+  {
+    "revision": "f008fb59803ca629cf05",
+    "url": "/reactToDoApp/static/js/runtime-main.f5fc6bfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactToDoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
